7fc4946c995ccfbbb0da00602cc2389c75b6ab70
Usama Ahmed <usiddiquie.bscs18seecs@seecs.edu.pk>
2019-11-30 12:57:16 +0500

    dataset path issue resolved.

--- config.py
+++ config.py
@@ -15,6 +15,4 @@ INVERTED_INDEX_BARRELS_PATH = os.path.join(PROJECT_PATH, "storage/inverted_index
 def dataset_files(batches_start=None, batches_end=None):
 	for batch in os.listdir(DATASET_PATH)[batches_start:batches_end]:
 		for file_path in os.listdir(os.path.join(DATASET_PATH, batch)):
-			yield file_path
-
-
+			yield os.path.join(DATASET_PATH, batch, file_path)
