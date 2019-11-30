fa9519a1c9ef219fff1ee6993b6a1b8b015bf9de
aftabakhtar <aftabakhtarnaveed@outlook.com>
2019-11-30 13:28:14 +0500

    changes in indexing/lexicon.py: removing links from texts

--- unit_tests/lexicon_tests.py
+++ unit_tests/lexicon_tests.py
@@ -5,7 +5,7 @@ from indexing.lexicon import Lexicon
 def main():
 	lexicon = Lexicon(config.LEXICON_PATH)
 
-	# lexicon.generate_lexicon(list(config.dataset_files(1)))
+	lexicon.generate_lexicon(list(config.dataset_files(1)))
 	# lexicon.update_lexicon(list(config.dataset_files()))
 	# lexicon_dict = lexicon.get_lexicon_dict()
 	# word_id = lexicon.get_word_id("inflation")
