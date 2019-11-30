3a082c62ce13e42dcd47bb9d104d681dda181a13
Usama Ahmed <usiddiquie.bscs18seecs@seecs.edu.pk>
2019-11-30 15:41:42 +0500

    Minor refactoring.

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
-from unit_tests import lexicon_tests, forward_index_tests
-
-
-if __name__ == '__main__':
-    lexicon_tests.main()
-    # When done with lexicon tests, uncomment next line
-#   # forward_index_tests.main()
-
-
 import unittest
 from unit_tests.test_inverted_index import TestInverteIndex 
 
-#if __name__ == '__main__':
-#   unittest.main()
+if __name__ == '__main__':
+  unittest.main()
